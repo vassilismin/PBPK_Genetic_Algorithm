a4534b279c232aaf4b15029d945db487d63d362f
Periklis_ts <ptsirostsib@gmail.com>
2022-05-20 16:11:38 +0300

    Test function for GA implementation
    Adding a test function script for testing the subroutines of the GA implementation.

---
+++ test_ga_functions.R
@@ -0,0 +1,9 @@
+binary_test <- x <- rbinom(n=104, size=1, prob=0.5) 
+print(binary_test)
+print(decode_ga(binary_test))
+
+par <- decode_ga(binary_test)
+P_groups <- length(unique(par[1:13]))  # sample size
+X_groups <- length(unique(par[14:length(par)]))  # sample size
+
+c <- par[2]  # acceptance number
